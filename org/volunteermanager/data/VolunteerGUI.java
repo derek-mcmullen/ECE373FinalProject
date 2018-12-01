2b5a8516b6b2be1e663ef7ed251d10091db266dd
dmcmullen <derek.m.mcmullen@gmail.com>
2018-12-01 15:45:32 -0700

    eventCreate coming together, added lots of comments

--- org/volunteermanager/data/VolunteerGUI.java
+++ org/volunteermanager/data/VolunteerGUI.java
@@ -9,6 +9,10 @@ import org.volunteermanager.people.*;
 
 
 public class VolunteerGUI extends JFrame{
+	
+	private JButton submitCreate; 
+	private JButton sendMessage; 
+	
 	 
     public VolunteerGUI() {
     	// GUI Title and size
@@ -17,14 +21,13 @@ public class VolunteerGUI extends JFrame{
     	
     	// Prep a tab  pane for navbar
     	JTabbedPane jtp = new JTabbedPane(); 
-    	ButtonHandler bhandler = new ButtonHandler();
     	
     	// Place the tabbedpane on the navbar
     	getContentPane().add(jtp); 
     	
     	// List of all tabs to be populated
     	JPanel joinPanel = new JPanel();
-    	JPanel createPanel = new JPanel(new GridLayout(4, 2));
+    	JPanel createPanel = new JPanel();
     	JPanel eventsPanel = new JPanel();
     	JPanel profilePanel = new JPanel();
     	JPanel messagingPanel = new JPanel(); 
@@ -45,37 +48,90 @@ public class VolunteerGUI extends JFrame{
     	
     	
     	
-		// Create panel implementation 
+		// Create Event panel implementation 
+    	createPanel.setLayout(new BorderLayout()); 
+    	
+    	JPanel innerPanel = new JPanel(new GridLayout(5,2)); 
+    	
 		JTextField eventName = new JTextField();
 	    JTextField eventLocation = new JTextField();
 	    JTextField eventInfo = new JTextField();
+	    JRadioButton skill1 = new JRadioButton(); 
+	    JRadioButton skill2 = new JRadioButton(); 
+	    JRadioButton skill3 = new JRadioButton(); 
+	    JRadioButton skill4 = new JRadioButton(); 
+	    JRadioButton interest1 = new JRadioButton(); 
+	    JRadioButton interest2 = new JRadioButton(); 
+	    JRadioButton interest3 = new JRadioButton(); 
+	    JRadioButton interest4 = new JRadioButton(); 
 	    
-	    createPanel.add(new JLabel("Event Title:"));
-	    createPanel.add(eventName);
-	    createPanel.add(new JLabel("Location:"));
-	    createPanel.add(eventLocation);
-	    createPanel.add(new JLabel("Extra Info:"));
-	    createPanel.add(eventInfo);
+	    // Skills options
+	    JPanel skillsPanel = new JPanel(new GridLayout(4,2));
+	    skillsPanel.setAlignmentX(RIGHT_ALIGNMENT);
+	    skillsPanel.add(skill1); 
+	    skillsPanel.add(new JLabel("Power Tools"));
+	    skillsPanel.add(skill2); 
+	    skillsPanel.add(new JLabel("Public Speaking"));
+	    skillsPanel.add(skill3); 
+	    skillsPanel.add(new JLabel("Childcare"));
+	    skillsPanel.add(skill4);
+	    skillsPanel.add(new JLabel("CPR Certified"));
+	    skillsPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+
+	    // Interests category
+	    JPanel interestsPanel = new JPanel(new GridLayout(4,2));
+	    interestsPanel.setAlignmentX(RIGHT_ALIGNMENT);
+	    interestsPanel.add(interest1); 
+	    interestsPanel.add(new JLabel("Construction"));
+	    interestsPanel.add(interest2); 
+	    interestsPanel.add(new JLabel("Politics"));
+	    interestsPanel.add(interest3); 
+	    interestsPanel.add(new JLabel("Education"));
+	    interestsPanel.add(interest4);
+	    interestsPanel.add(new JLabel("Children"));
+	    interestsPanel.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
+
+	    // innerPanel is the center of the createPanel tab
+	    innerPanel.add(new JLabel("Event Title:"));
+	    innerPanel.add(eventName);
+	    innerPanel.add(new JLabel("Location:"));
+	    innerPanel.add(eventLocation);
+	    innerPanel.add(new JLabel("Extra Info:"));
+	    innerPanel.add(eventInfo);
+	    innerPanel.add(new JLabel("Required Skills:"));
+	    innerPanel.add(skillsPanel);
+	    innerPanel.add(new JLabel("Interest Category:"));
+	    innerPanel.add(interestsPanel);
+	   
+	    createPanel.add(innerPanel, BorderLayout.CENTER); 
     	
-    	JButton submitCreate = new JButton("Create Event");
-    	createPanel.add(submitCreate);
-    	submitCreate.addActionListener(bhandler);
+    	submitCreate = new JButton("Create Event");
+    	createPanel.add(submitCreate, BorderLayout.SOUTH);
+    	submitCreate.addActionListener(new ButtonHandler());
     	setVisible(true);
     	
     	
     	
 	    
 		// Messaging panel implementation 
-    	JButton test = new JButton("Send Message");
-    	messagingPanel.add(test);
+    	sendMessage = new JButton("Send Message");
+    	messagingPanel.add(sendMessage);
 
-    	test.addActionListener(bhandler);
+    	sendMessage.addActionListener(new ButtonHandler());
     	setVisible(true);
     }
  
     class ButtonHandler implements ActionListener{
     	public void actionPerformed(ActionEvent e){
-    		 JOptionPane.showMessageDialog(null, "I've been pressed", "What happened?", JOptionPane. INFORMATION_MESSAGE);
+    		
+    		JButton source = (JButton)(e.getSource());
+    		
+    		// IF-ELSE tree to check which button was pressed (all buttons must be declared as private fields of the overall class to be readable here)
+    		if (source.equals(submitCreate)) { 
+    			JOptionPane.showMessageDialog(null, "Event Created Button", "What happened?", JOptionPane. INFORMATION_MESSAGE);
+    		} else if (source.equals(sendMessage)) { 
+    			JOptionPane.showMessageDialog(null, "sendMessage button has been clicked", "What happened?", JOptionPane. INFORMATION_MESSAGE);
+    		}
     	}
     }
     
